5e2a530fa12077b7b00a5814eb5c178d07f53c6f
wang_changyuan <wang_changyuan@microad-tech.com>
2016-12-23 13:01:07 +0800

    显示tooltip

--- WebContent/WEB-INF/views/js/barGraph.js
+++ WebContent/WEB-INF/views/js/barGraph.js
@@ -57,7 +57,6 @@ define([ "common" ], function(common) {
                 tickDecimals: 0,
                 ticks: inforNameTmp
             },
-            yaxis:{ tickDecimals: 2},
             colors: [ "#1ab394" ],
             grid: {
                 color: "#999999",
@@ -69,19 +68,51 @@ define([ "common" ], function(common) {
             legend: {
                 show: !1
             },
-            tooltip: true,
-            tooltipOpts: {
-                content: "%y" + "%"
-            }
         }
         var barData = [ {
-            label: "bar",
-            data: rateTmp,
-            color: "red"
+            label: "胜率",
+            data: rateTmp
         } ];
         $.plot($("#flot-bar-chart"), barData, options);
+        $("#flot-bar-chart").UseTooltip();
     }
     ;
+
+    var previousPoint = null, previousLabel = null;
+    $.fn.UseTooltip = function() {
+        $(this).bind("plothover", function(event, pos, item) {
+            if (item) {
+                if ((previousLabel != item.series.label) || (previousPoint != item.dataIndex)) {
+                    previousPoint = item.dataIndex;
+                    previousLabel = item.series.label;
+                    $("#tooltip").remove();
+
+                    var x = item.datapoint[0];
+                    var y = item.datapoint[1];
+                    var color = item.series.color;
+                    showTooltip(item.pageX, item.pageY, color, "<strong>" + item.series.label + "</strong> : <strong>" + y + "</strong> ");
+                }
+            } else {
+                $("#tooltip").remove();
+                previousPoint = null;
+            }
+        });
+    };
+    function showTooltip(x, y, color, contents) {
+        $('<div id="tooltip">' + contents + '%</div>').css({
+            position: 'absolute',
+            display: 'none',
+            top: y - 40,
+            left: x - 20,
+            border: '2px solid ' + color,
+            padding: '3px',
+            'font-size': '9px',
+            'border-radius': '5px',
+            'background-color': '#fff',
+            'font-family': 'Verdana, Arial, Helvetica, Tahoma, sans-serif',
+            opacity: 0.9
+        }).appendTo("body").fadeIn(200);
+    }
     /**
      * 取路径
      */
